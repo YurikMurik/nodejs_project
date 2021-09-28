848b61068d4a837deb5cfbd660361cec68634540
Yuri Lysov <yuri_lysov@epam.com>
2021-09-29 00:52:36 +0300

    task1 + minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+import es from 'event-stream';
+
+// TASK 1
+process.stdin
+  .pipe(es.split())
+  .pipe(es.mapSync((data: string) => 
+    data.split('').reverse().join('').trim()
+  ))
+  .pipe(process.stdout);
