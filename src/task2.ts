c8a0d10f8372c277749b889b70123291ddb092cf
Yuri Lysov <yuri_lysov@epam.com>
2021-10-03 16:40:56 +0300

    finish task2, delete useless files, another fixes

--- src/task2.ts
+++ src/task2.ts
@@ -1,27 +1,20 @@
-const csv = require("csvtojson");
-const { pipeline } = require("stream");
-const fs = require("fs");
-const path = require("path");
+import csv from "csvtojson";
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream";
 
 const csvPath = path.join(__dirname, "./csv/nodejs-hw1-ex1.csv");
-const jsonPath = path.join(__dirname, "./csv/example.json");
+const jsonPath = path.join(__dirname, "./csv/result.txt");
 
-// pipeline(
-//   fs.createReadStream(csvPath),
-//   csv(),
-//   fs.createWriteStream(jsonPath),
-//   (err: any) => {
-//     if (err) {
-//       console.error("Pipeline failed.", err);
-//     } else {
-//       console.log("Pipeline succeeded.");
-//     }
-//   },
-// );
-
-csv()
-  .fromFile(csvPath)
-  .on("json", (jsonObj: any) => {
-    //when parse finished, result will be emitted here.
-    console.log(jsonObj);
-  });
+pipeline(
+  fs.createReadStream(csvPath),
+  csv(),
+  fs.createWriteStream(jsonPath),
+  (err: any) => {
+    if (err) {
+      console.error("Pipeline failed.", err);
+    } else {
+      console.log("Pipeline succeeded.");
+    }
+  },
+);
